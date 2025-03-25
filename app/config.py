58e0bbb7a188cd42292dbaf0d7cdb4896fd2d89f
foadjal <parhylfoadjo@gmail.com>
2025-03-25 09:21:34 -0400

    mise a jour avec toutes les tech securité

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 # app/config.py
 import os
+from dotenv import load_dotenv
+
+
+
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')  # à changer en prod
+    UPLOAD_FOLDER = 'generated/'
