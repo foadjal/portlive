4829ac30165d57369d68dc4d24a4e48a7528838d
foadjal <parhylfoadjo@gmail.com>
2025-03-25 09:21:34 -0400

    mise a jour avec toutes les tech securité

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,26 @@ from app.routes.dk import dk_bp
 from app.routes.havre import havre_bp
 from app.routes.home import home_bp
 from app.routes.download import download_bp
+from flask_wtf import CSRFProtect
+from flask_talisman import Talisman
 
+
+csrf = CSRFProtect()
 socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
+    Talisman(app,
+             content_security_policy=None,
+             force_https=False,)
     app.config.from_object('app.config.Config')
+    csrf.init_app(app)
+
+    @app.context_processor
+    def inject_csrf():
+        from flask_wtf.csrf import generate_csrf
+        return dict(csrf_token=lambda: f'<input type="hidden" name="csrf_token" value="{generate_csrf()}">')
+
     app.register_blueprint(dk_bp, url_prefix='/dk_port')
     app.register_blueprint(havre_bp, url_prefix='/havre_port')
     app.register_blueprint(home_bp)
@@ -20,3 +34,4 @@ def create_app():
     app.register_blueprint(download_bp)
 
     return app
+
